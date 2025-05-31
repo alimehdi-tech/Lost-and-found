3c4a2729c53c45cb541b0bca0896d64e954c6a3b
Muhammad <abdullahgu77ar@gmail.com>
2025-05-31 14:58:52 +0500

    lost page progress

--- src/app/items/lost/page.js
+++ src/app/items/lost/page.js
@@ -194,9 +194,9 @@ export default function LostItems() {
                 </select>
               </div>
 
-              <button type="submit" className="btn-primary">
+              <button type="submit" className="flex items-center bg-black text-white hover:bg-gray-900 px-4 py-2 rounded">
                 <FunnelIcon className="h-4 w-4 mr-2" />
-                Apply Filters
+                  Apply Filters
               </button>
             </div>
           </form>
